9092e96f33e9a9a1dbc92884576026d32d02fdad
leszrom <romanowski.leszek@gmail.com>
2019-01-26 17:00:32 +0100

    Map relation @ManyToMany between Members and Permisions

---
+++ src/main/java/com/crud/events/domain/Member.java
@@ -0,0 +1,30 @@
+package com.crud.events.domain;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity(name = "Members")
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ID")
+    private long id;
+
+    @Column(name = "Firstname")
+    private String firstname;
+
+    @Column(name = "Lastname")
+    private String lastname;
+
+    @ManyToMany(cascade = CascadeType.PERSIST)
+    @JoinTable(
+            name = "Members_Permissions",
+            joinColumns = {@JoinColumn(name = "MemberID", referencedColumnName = "ID")},
+            inverseJoinColumns = {@JoinColumn(name = "PermissionID", referencedColumnName = "ID")}
+    )
+    private List<Permission> permissions = new ArrayList<>();
+
+    public Member() {
+    }
+}
---
+++ src/main/java/com/crud/events/domain/Permission.java
@@ -0,0 +1,22 @@
+package com.crud.events.domain;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity(name = "Permissions")
+public class Permission {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ID")
+    private long id;
+
+    @Column(name = "Role")
+    private String role;
+
+    @ManyToMany(mappedBy = "permissions")
+    private List<Member> members = new ArrayList<>();
+
+    public Permission() {
+    }
+}
