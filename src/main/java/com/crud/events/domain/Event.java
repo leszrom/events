ad317c2048a2ec02898871a5a1c2a009b61bee50
leszrom <romanowski.leszek@gmail.com>
2019-01-26 17:23:48 +0100

    Map relation @ManyToMany between Events and Members

---
+++ src/main/java/com/crud/events/domain/Event.java
@@ -0,0 +1,28 @@
+package com.crud.events.domain;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ID")
+    private long id;
+
+    @Column(name = "Name")
+    private String name;
+
+    @Column(name = "Description")
+    private String description;
+
+    @ManyToMany(cascade = CascadeType.PERSIST)
+    @JoinTable(
+            name = "Events_Members",
+            joinColumns = @JoinColumn(name = "EventID", referencedColumnName = "ID"),
+            inverseJoinColumns = @JoinColumn(name = "MemberID", referencedColumnName = "ID"))
+    private List<Member> members = new ArrayList<>();
+
+    public Event() {
+    }
+}
--- src/main/java/com/crud/events/domain/Member.java
+++ src/main/java/com/crud/events/domain/Member.java
@@ -25,6 +25,9 @@ public class Member {
     )
     private List<Permission> permissions = new ArrayList<>();
 
+    @ManyToMany(mappedBy = "members")
+    private List<Event> events = new ArrayList<>();
+
     public Member() {
     }
 }
