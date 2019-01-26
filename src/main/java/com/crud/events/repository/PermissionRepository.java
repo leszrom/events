bf64059fe44ed531e08ad1342cf573d9c6a3b1b6
leszrom <romanowski.leszek@gmail.com>
2019-01-26 18:53:28 +0100

    Add CRUD repositories

---
+++ src/main/java/com/crud/events/repository/PermissionRepository.java
@@ -0,0 +1,9 @@
+package com.crud.events.repository;
+
+import com.crud.events.domain.Permission;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PermissionRepository extends CrudRepository<Permission, Long> {
+}
