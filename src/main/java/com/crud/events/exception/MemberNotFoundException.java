9f7c16855d830d1bc4746c7f1eee4bd3b3c6ec0d
leszrom <romanowski.leszek@gmail.com>
2019-01-27 15:17:36 +0100

    Test service 'getMember' by given id method

--- src/main/java/com/crud/events/exception/MemberNotFoundException.java
+++ src/main/java/com/crud/events/exception/MemberNotFoundException.java
@@ -2,6 +2,6 @@ package com.crud.events.exception;
 
 public class MemberNotFoundException extends RuntimeException {
     public MemberNotFoundException() {
-        super("The Member with the given id does not exist");
+        super("The Member with the given ID doesn't exist!");
     }
 }
--- src/test/java/com/crud/events/service/MemberServiceTestSuite.java
+++ src/test/java/com/crud/events/service/MemberServiceTestSuite.java
@@ -2,6 +2,8 @@ package com.crud.events.service;
 
 import com.crud.events.domain.Member;
 import com.crud.events.domain.dto.MemberRequest;
+import com.crud.events.domain.dto.MemberResponse;
+import com.crud.events.exception.MemberNotFoundException;
 import com.crud.events.mapper.MemberMapper;
 import com.crud.events.repository.MemberRepository;
 import org.junit.Assert;
@@ -11,6 +13,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.Optional;
+
+import static com.googlecode.catchexception.CatchException.caughtException;
+import static com.googlecode.catchexception.CatchException.verifyException;
 
 @RunWith(MockitoJUnitRunner.class)
 public class MemberServiceTestSuite {
@@ -42,13 +49,35 @@ public class MemberServiceTestSuite {
     }
 
     @Test
-    public void should_return_GetMemberById() {
+    public void should_return_member_by_given_id() {
         //Given
+        Member member = new Member("Firstname", "Lastname");
+        MemberResponse memberResponse = new MemberResponse(1L, "Firstname", "Lastname");
+
+        Mockito.when(memberRepository.findById(1L)).thenReturn(Optional.of(member));
+        Mockito.when(memberMapper.mapToMemberResponse(member)).thenReturn(memberResponse);
 
         //When
+        MemberResponse readMember = memberService.getMemberById(1L);
 
         //Then
+        Assert.assertEquals(1L, readMember.getId());
+        Assert.assertEquals("Firstname", readMember.getFirstname());
+        Assert.assertEquals("Lastname", readMember.getLastname());
+    }
 
+    @Test
+    public void should_throw_exception_when_member_with_given_id_does_not_exist() {
+        //Given
+        String exceptionMessage = "The Member with the given ID doesn't exist!";
+        Mockito.when(memberRepository.findById(1L)).thenThrow(new MemberNotFoundException());
+
+        //When
+        verifyException(memberService).getMemberById(1L);
+
+        //Then
+        Assert.assertEquals(MemberNotFoundException.class, caughtException().getClass());
+        Assert.assertEquals(exceptionMessage, caughtException().getMessage());
     }
 
     @Test
