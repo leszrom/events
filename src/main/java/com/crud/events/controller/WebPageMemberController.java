4ccd0d6f0e64f1fb98b3c3adf21d862290fa40e7
leszrom <romanowski.leszek@gmail.com>
2019-02-25 17:35:32 +0100

    Rename WebPage Controller and Service

--- src/main/java/com/crud/events/controller/WebPageMemberController.java
+++ src/main/java/com/crud/events/controller/WebPageMemberController.java
@@ -1,6 +1,6 @@
 package com.crud.events.controller;
 
-import com.crud.events.service.WebPageService;
+import com.crud.events.service.WebPageMemberService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,17 +8,17 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("v1/front/")
-public class WebPageController {
-    private WebPageService webPageService;
+public class WebPageMemberController {
+    private WebPageMemberService webPageMemberService;
 
-    public WebPageController(WebPageService webPageService) {
-        this.webPageService = webPageService;
+    public WebPageMemberController(WebPageMemberService webPageMemberService) {
+        this.webPageMemberService = webPageMemberService;
     }
 
     @RequestMapping(value = "members", method = RequestMethod.GET)
     public ModelAndView getMembersView() {
         ModelAndView modelAndView = new ModelAndView("home");
-        modelAndView.addObject("members", webPageService.getAllMembers());
+        modelAndView.addObject("members", webPageMemberService.getAllMembers());
         return modelAndView;
     }
 }
--- src/main/java/com/crud/events/service/WebPageMemberService.java
+++ src/main/java/com/crud/events/service/WebPageMemberService.java
@@ -1,7 +1,6 @@
 package com.crud.events.service;
 
 import com.crud.events.domain.Member;
-import com.crud.events.repository.EventRepository;
 import com.crud.events.repository.MemberRepository;
 import com.crud.events.repository.PermissionRepository;
 import org.springframework.stereotype.Service;
@@ -10,13 +9,11 @@ import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
-public class WebPageService {
-    private EventRepository eventRepository;
+public class WebPageMemberService {
     private MemberRepository memberRepository;
     private PermissionRepository permissionRepository;
 
-    public WebPageService(EventRepository eventRepository, MemberRepository memberRepository, PermissionRepository permissionRepository) {
-        this.eventRepository = eventRepository;
+    public WebPageMemberService(MemberRepository memberRepository, PermissionRepository permissionRepository) {
         this.memberRepository = memberRepository;
         this.permissionRepository = permissionRepository;
     }
