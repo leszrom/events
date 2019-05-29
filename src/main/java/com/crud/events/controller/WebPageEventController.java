b9e049d8ce21b71abbd6bd2a81ce139da0bbde19
leszrom <romanowski.leszek@gmail.com>
2019-05-29 11:34:49 +0200

    Add 'create event' form to 'events' view

--- src/main/java/com/crud/events/controller/WebPageEventController.java
+++ src/main/java/com/crud/events/controller/WebPageEventController.java
@@ -5,6 +5,7 @@ import com.crud.events.domain.Member;
 import com.crud.events.service.WebPageEventService;
 import com.crud.events.service.WebPageMemberService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -31,8 +32,18 @@ public class WebPageEventController {
     public ModelAndView getEventsView() {
         ModelAndView modelAndView = new ModelAndView("events");
         modelAndView.addObject("events", webPageEventService.getAllEvents());
+        modelAndView.addObject("event", new Event());
         return modelAndView;
     }
+
+    @RequestMapping(value = "events", method = RequestMethod.POST)
+    public ModelAndView createEvent(@ModelAttribute Event event) {
+        webPageEventService.saveNewEvent(event);
+        ModelAndView modelAndView = new ModelAndView("events");
+        modelAndView.addObject("events", webPageEventService.getAllEvents());
+        return modelAndView;
+    }
+
 
     @RequestMapping(method = RequestMethod.GET, value = "events/{id}")
     public ModelAndView getEventView(@PathVariable Long id) {
--- src/main/java/com/crud/events/service/WebPageEventService.java
+++ src/main/java/com/crud/events/service/WebPageEventService.java
@@ -1,19 +1,25 @@
 package com.crud.events.service;
 
 import com.crud.events.domain.Event;
+import com.crud.events.domain.Member;
 import com.crud.events.exception.EventNotFoundException;
 import com.crud.events.repository.EventRepository;
+import com.crud.events.repository.MemberRepository;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.List;
 
+import static com.crud.events.domain.Role.VIP;
+
 @Service
 public class WebPageEventService {
     private final EventRepository eventRepository;
+    private final MemberRepository memberRepository;
 
-    public WebPageEventService(EventRepository eventRepository) {
+    public WebPageEventService(EventRepository eventRepository, MemberRepository memberRepository) {
         this.eventRepository = eventRepository;
+        this.memberRepository = memberRepository;
     }
 
     @Transactional
@@ -25,5 +31,12 @@ public class WebPageEventService {
     public Event getEventById(final Long id) {
         return eventRepository.findById(id)
                 .orElseThrow(EventNotFoundException::new);
+    }
+
+    @Transactional
+    public void saveNewEvent(final Event event) {
+        List<Member> members = memberRepository.retrieveMembersWithRole(VIP);
+        members.forEach(member -> event.getMembers().add(member));
+        eventRepository.save(event);
     }
 }
